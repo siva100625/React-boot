34cd96beb32d0828b101d570fc3bfdce00523bbf
siva100625 <sivasubramanian.v2023ece@sece.ac.ingamil.com>
2025-07-18 00:46:16 +0530

    day13

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,20 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import GetEmployees from "./components/GetEmployees";
+import HomePage from "./components/HomePage";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import HomePage from "./components/HomePage";
 import AddEmployees from "./components/AddEmployees";
-
+import GetEmployees from "./components/GetEmployees";
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/add" element={<AddEmployees />} />
-      <Route path="/getemployees" element={<GetEmployees/>}/>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Signup />} />
-      <Route path="/addemployee"element={<Signup/>}></Route>
+      <Route path="/add" element={<AddEmployees />} />
+      <Route path="/getemployees" element={<GetEmployees />} />
     </Routes>
   </Router>
 );
